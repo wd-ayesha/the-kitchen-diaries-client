6eac80cc15e5845260dcca1d2b5c10753a72c50f
Ayesha <ayesha18mbstu@gmail.com>
2023-05-03 11:32:09 +0600

    header & footer added

--- src/pages/Shared/Footer/Footer.jsx
+++ src/pages/Shared/Footer/Footer.jsx
@@ -1,10 +1,97 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
+import { FaFacebook, FaGithub, FaGoogle } from 'react-icons/fa';
+
 
 const Footer = () => {
     return (
-        <div>
-            <h3>@ Copywright</h3>
+
+<div className="Container my-5">
+
+  <footer className="text-white text-center text-lg-start bg-dark">
+   
+    <div className="container p-4">
+    
+      <div className="row mt-4">
+      
+        <div className="col-lg-4 col-md-12 mb-4 mb-md-0">
+          <h5 className="text-uppercase mb-4">About The Kitchen Diaries</h5>
+
+          <p>
+          The Kitchen Diaries is an excellent resource for food recipe companies looking to create delicious, seasonal recipes that are simple, accessible, and flavorful.
+          </p>
+
+          <p>
+          You can use The Kitchen Diaries as a starting point for creating your own recipe ideas to suit your customers' preferences and dietary requirements.
+          </p>
+
+          <p>Additionally, you can also use The Kitchen Diaries as a source of inspiration for creating seasonal menus.</p>
+
         </div>
+       
+        <div className="col-lg-4 col-md-6 mb-4 mb-md-0">
+          <h5 className="text-uppercase mb-4 pb-1">Search something</h5>
+
+          <div className="form-outline form-white mb-4">
+            <input type="text" id="formControlLg" className="form-control form-control-lg" placeholder='Search' />
+          </div>
+
+          <ul className="fa-ul" style={{marginRight: '1.5em'}}>
+            <li className="mb-3">
+              <span className="fa-li"><i className="fas fa-home"></i></span><span className="ms-2">Warsaw, 00-967, Poland</span>
+            </li>
+            <li className="mb-3">
+              <span className="fa-li"><i className="fas fa-envelope"></i></span><span className="ms-2">contact@example.com</span>
+            </li>
+            <li className="mb-3">
+              <span className="fa-li"><i className="fas fa-phone"></i></span><span className="ms-2">+ 48 234 567 88</span>
+            </li>
+          </ul>
+          <div className="mt-4">
+     
+            <a type="button" className="btn btn-floating btn-light btn-lg me-2"><FaFacebook /></a>
+
+            <a type="button" className="btn btn-floating btn-light btn-lg me-2"><FaGithub /></a>
+
+            <a type="button" className="btn btn-floating btn-light btn-lg"><FaGoogle /></a>
+        
+          </div>
+        </div>
+      
+        <div className="col-lg-4 col-md-6 mb-4 mb-md-0">
+          <h5 className="text-uppercase mb-4">Opening hours</h5>
+
+          <table className="table text-center text-white">
+            <tbody className="fw-normal">
+              <tr>
+                <td>Mon - Thu:</td>
+                <td>8am - 9pm</td>
+              </tr>
+              <tr>
+                <td>Fri - Sat:</td>
+                <td>8am - 1am</td>
+              </tr>
+              <tr>
+                <td>Sunday:</td>
+                <td>9am - 10pm</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+    
+      </div>
+
+    </div>
+   
+    <div className="text-center p-3" style={{backgroundColor: 'rgba(0, 0, 0, 0.2)'}}>
+      © 2020 Copyright:
+      <a className="text-white text-decoration-none" href="#"> thekitchendiaries.com</a>
+    </div>
+
+  </footer>
+
+</div>
+
     );
 };
 
--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import { Container, Image, Nav, Navbar } from 'react-bootstrap';
 
 const Header = () => {
     return (
-        <div>
-            <h3>Header</h3>
-        </div>
+        <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">The Kitchen Diaries</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Blog</Nav.Link>
+            <Nav.Link href="#link"><Image src="holder.js/171x180" roundedCircle /></Nav.Link>
+            <Nav.Link href="#link">Login</Nav.Link>
+            <Nav.Link href="#link">Register</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
     );
 };
 
